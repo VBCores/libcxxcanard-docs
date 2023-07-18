45b4655abad79391437569fc9d8ca737d12cb822
Igor Beschastnov <beschastnovigor@gmail.com>
2023-07-18 18:30:14 +0300

    linux example

--- examples/hbeat-linux/heartbeat_sub.cpp
+++ examples/hbeat-linux/heartbeat_sub.cpp
@@ -4,23 +4,44 @@
 #include "cyphal/subscriptions/subscription.h"
 
 #include <iostream>
+#include <chrono>
 
 #include "uavcan/node/Heartbeat_1_0.h"
 
-void Error_Handler() {
-    std::cout << "err" << std::endl;
-}
+void Error_Handler() {std::cout << "error" << std::endl; while(1){}}
 
 SUBSCRIPTION_CLASS_FIXED_MESSAGE(HBeatReader, uavcan_node_Heartbeat_1_0)
 void HBeatReader::handler(const uavcan_node_Heartbeat_1_0& hbeat, CanardRxTransfer* transfer) {
-    std::cout << hbeat.uptime << " " << +transfer->metadata.remote_node_id << std::endl;
+    std::cout << +transfer->metadata.remote_node_id << ": " << hbeat.uptime <<  std::endl;
+}
+
+CyphalInterface* interface;
+
+uint32_t uptime = 0;
+PREPARE_MESSAGE(uavcan_node_Heartbeat_1_0, hbeat)
+void heartbeat() {
+    uavcan_node_Heartbeat_1_0 heartbeat_msg = {.uptime = uptime, .health = {uavcan_node_Health_1_0_NOMINAL}, .mode = {uavcan_node_Mode_1_0_OPERATIONAL}};
+    interface->SEND_MSG(uavcan_node_Heartbeat_1_0, &heartbeat_msg, hbeat_buf, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_, &hbeat_transfer_id);
+    uptime += 1;
+}
+
+uint64_t current_time_millis() {
+    using namespace std::chrono;
+    return duration_cast<milliseconds>(system_clock::now()).count();
 }
 
 int main() {
-    auto interface = CyphalInterface(99);
+    interface = new CyphalInterface(99);
     interface.setup<LinuxCAN, O1Allocator>("can0");
-    auto reader = HBeatReader(&interface);
+    auto reader = HBeatReader(interface);
+
+    auto last_hbeat = current_time_millis();
     while (1) {
-        interface.loop();
+        interface->loop();
+        auto now = current_time_millis();
+        if ( (now - last_hbeat) >= 1000) {
+            last_hbeat = now;
+            heartbeat();
+        }
     }
 }
