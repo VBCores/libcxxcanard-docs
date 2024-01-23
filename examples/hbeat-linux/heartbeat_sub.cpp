c61b6463c07c357a9b80438e6d3c9c98cdb292c1
Igor Beschastnov <beschastnovigor@gmail.com>
2024-01-23 17:38:44 +0300

    template lib

--- examples/hbeat-linux/heartbeat_sub.cpp
+++ examples/hbeat-linux/heartbeat_sub.cpp
@@ -9,41 +9,48 @@
 #include "cyphal/subscriptions/subscription.h"
 
 #include "uavcan/node/Heartbeat_1_0.h"
+TYPE_ALIAS(HBeat, uavcan_node_Heartbeat_1_0)
 
-std::byte buffer[sizeof(CyphalInterface) + sizeof(LinuxCAN) + sizeof(O1Allocator)];
-std::shared_ptr<CyphalInterface> interface;
 
 void error_handler() {std::cout << "error" << std::endl; std::exit(EXIT_FAILURE);}
 
-uint64_t timeMillis() {
+uint64_t micros_64() {
     using namespace std::chrono;
     return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 }
 
-SUBSCRIPTION_CLASS_FIXED_MESSAGE(HBeatReader, uavcan_node_Heartbeat_1_0)
-void HBeatReader::handler(const uavcan_node_Heartbeat_1_0& hbeat, CanardRxTransfer* transfer) {
-    std::cout << +transfer->metadata.remote_node_id << ": " << hbeat.uptime <<  std::endl;
-}
+UtilityConfig utilities(micros_64, error_handler);
+std::byte buffer[sizeof(CyphalInterface) + sizeof(LinuxCAN) + sizeof(O1Allocator)];
+std::shared_ptr<CyphalInterface> interface;
 
+class HBeatReader: public AbstractSubscription<HBeat> {
+public:
+    HBeatReader(InterfacePtr interface): AbstractSubscription<HBeat>(interface, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_) {};
+    void handler(const uavcan_node_Heartbeat_1_0& hbeat, CanardRxTransfer* transfer) override {
+        std::cout << +transfer->metadata.remote_node_id << ": " << hbeat.uptime <<  std::endl;
+    }
+};
+
+uint8_t hbeat_buffer[HBeat::buffer_size];
+CanardTransferID hbeat_transfer_id = 0;
 uint32_t uptime = 0;
-PREPARE_MESSAGE(uavcan_node_Heartbeat_1_0, hbeat)
 void heartbeat() {
-    uavcan_node_Heartbeat_1_0 heartbeat_msg = {.uptime = uptime, .health = {uavcan_node_Health_1_0_NOMINAL}, .mode = {uavcan_node_Mode_1_0_OPERATIONAL}};
-    interface->SEND_MSG(uavcan_node_Heartbeat_1_0, &heartbeat_msg, hbeat_buf, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_, &hbeat_transfer_id);
+    HBeat::Type heartbeat_msg = {.uptime = uptime, .health = {uavcan_node_Health_1_0_NOMINAL}, .mode = {uavcan_node_Mode_1_0_OPERATIONAL}};
+    interface->send_cyphal_default_msg<HBeat>(&heartbeat_msg, hbeat_buffer, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_, &hbeat_transfer_id);
     uptime += 1;
 }
 
 int main() {
     interface = std::shared_ptr<CyphalInterface>(
-        CyphalInterface::create<LinuxCAN, O1Allocator>(buffer, 100, "can0", 1000)  // 1000 msgs combined pool
+        CyphalInterface::create<LinuxCAN, O1Allocator>(buffer, 100, "can0", 1000, utilities)  // 1000 msgs combined pool
     );
 
     auto reader = HBeatReader(interface);
 
-    auto last_hbeat = timeMillis();
+    auto last_hbeat = micros_64();
     while (1) {
         interface->loop();
-        auto now = timeMillis();
+        auto now = micros_64();
         if ( (now - last_hbeat) >= 1000) {
             std::cout << "Sending heartbeat " << uptime << ". Queue size: " << interface->queue_size() << std::endl;
             last_hbeat = now;
