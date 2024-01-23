6443d1073a3e3a4ad9ac9fc6a3cbc0cbc4c48ef6
Igor Beschastnov <beschastnovigor@gmail.com>
2024-01-23 17:48:49 +0300

    some comments

--- examples/hbeat-linux/heartbeat_sub.cpp
+++ examples/hbeat-linux/heartbeat_sub.cpp
@@ -11,30 +11,31 @@
 #include "uavcan/node/Heartbeat_1_0.h"
 TYPE_ALIAS(HBeat, uavcan_node_Heartbeat_1_0)
 
-
 void error_handler() {std::cout << "error" << std::endl; std::exit(EXIT_FAILURE);}
-
 uint64_t micros_64() {
     using namespace std::chrono;
     return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 }
-
 UtilityConfig utilities(micros_64, error_handler);
+
 std::byte buffer[sizeof(CyphalInterface) + sizeof(LinuxCAN) + sizeof(O1Allocator)];
 std::shared_ptr<CyphalInterface> interface;
 
 class HBeatReader: public AbstractSubscription<HBeat> {
 public:
-    HBeatReader(InterfacePtr interface): AbstractSubscription<HBeat>(interface, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_) {};
+    HBeatReader(InterfacePtr interface): AbstractSubscription<HBeat>(interface,
+        // Тут параметры - port_id, transfer kind или только port_id
+        uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_
+    ) {};
     void handler(const uavcan_node_Heartbeat_1_0& hbeat, CanardRxTransfer* transfer) override {
         std::cout << +transfer->metadata.remote_node_id << ": " << hbeat.uptime <<  std::endl;
     }
 };
 
-uint8_t hbeat_buffer[HBeat::buffer_size];
-CanardTransferID hbeat_transfer_id = 0;
 uint32_t uptime = 0;
 void heartbeat() {
+    static uint8_t hbeat_buffer[HBeat::buffer_size];
+    static CanardTransferID hbeat_transfer_id = 0;
     HBeat::Type heartbeat_msg = {.uptime = uptime, .health = {uavcan_node_Health_1_0_NOMINAL}, .mode = {uavcan_node_Mode_1_0_OPERATIONAL}};
     interface->send_cyphal_default_msg<HBeat>(&heartbeat_msg, hbeat_buffer, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_, &hbeat_transfer_id);
     uptime += 1;
